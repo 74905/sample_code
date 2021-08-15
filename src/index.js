d1b5f1169ac5eb4aa1fb869364b899176ef29de1
74905 <73700673+74905@users.noreply.github.com>
2021-08-15 20:10:22 +0900

    opp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 /**
  * cont let
  */
-var sas = "var変数";
+// var sas = "var変数";
 
-sas = "uhahaha";
+// sas = "uhahaha";
 
-var sas = 111;
-console.log(sas);
+// var sas = 111;
+
+// let kk = "sasas";
+// console.log(kk);
+
+// const オブジェト定義　プロパティの変更が可能
+// const val4 = {
+//   name: "あまま",
+//   age: 28
+// };
+
+// val4.name = "ゆめみ";
+// val4.nnn = "冬樹";
+// console.log(val4);
